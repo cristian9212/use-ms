abfd0fcbea5ebd9e729b09f39208c4743e7ae7dd
Cristian <ing.talero@gmail.com>
2026-09-24 19:43:00 -0500

    Nuevo

---
+++ use-ms/src/test/java/com/example/use_ms/UseMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.use_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
